c06a0d10bfbdb2a907b3ed701642c71cbee922d6
Denis Mikhaylov <d.mikhaylov@qiwi.ru>
2013-12-24 09:17:27 +0400

    Send didMoveToParentViewController: to the childTableViewController to fulfill view controller containment contract

--- GroceryList/GCYTableViewController.m
+++ GroceryList/GCYTableViewController.m
@@ -24,7 +24,7 @@
 
 	_childTableViewController = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
 	[self addChildViewController:self.childTableViewController];
-
+	[self.childTableViewController didMoveToParentViewController:self];
 	return self;
 }
 
